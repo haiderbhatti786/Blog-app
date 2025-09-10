673669b8b19ac2c4883b660c583abfa6b792ff1d
haiderbhatti786 <haiderbhatti021@gmail.com>
2025-09-10 11:19:33 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ import multer from "multer";
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Frontend ka origin
+    origin: "https://blog-app-clinet.vercel.app/", // Frontend ka origin
     credentials: true, // Agar cookies ya headers send karne hain to true rakho
   })
 );
